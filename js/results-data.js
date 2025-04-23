e79eb3dc8beef92ee76bacd38adff420c358ebdc
Jonathan Bossenger <jonathanbossenger@gmail.com>
2025-04-23 10:37:48 +0200

    Updating results from 2025-04-14

--- js/results-data.js
+++ js/results-data.js
@@ -5,12 +5,44 @@ const RESULTS_DATA = {
             event: "Curro Durbanville",
             results: [
                 {
+                    team: "u10 Girls A",
+                    score: "8-0"
+                },
+                {
+                    team: "u10 Girls B",
+                    score: "3-0"
+                },
+                {
+                    team: "u11 Girls A",
+                    score: "10-0"
+                },
+                {
+                    team: "u13 Girls A",
+                    score: "4-0"
+                },
+                {
                     team: "u10 Boys A",
                     score: "3-0"
                 },
                 {
                     team: "u10 Boys B",
+                    score: "1-0"
+                },
+                {
+                    team: "u11 Boys A",
+                    score: "3-0"
+                },
+                {
+                    team: "u11 Boys B",
+                    score: "1-2"
+                },
+                {
+                    team: "u13 Boys A",
                     score: "2-0"
+                },
+                {
+                    team: "u13 Boys B",
+                    score: "1-0"
                 }
             ]
         }
